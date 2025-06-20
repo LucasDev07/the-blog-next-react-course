8625ec2862f700ee5473fe24c5d2419d538a42f6
Lucas Vargas <lukeamfernandes@gmail.com>
2025-06-20 18:47:01 -0300

    initial

---
+++ src/app/about/me/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return <h1>Me</h1>
+}
+
+// page.tsx = index.html
+// app/page.tsx -> /
+// app/about/page.tsx -> /about 
---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutMePage() {
+  return <h1>About</h1>
+}
+
+// page.tsx = index.html
+// app/page.tsx -> /
+// app/about/page.tsx -> /about 
