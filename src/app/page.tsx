23f2178d7964255f07672b0411f24824c82efa1f
Lucas <lukeamfernandes@gmail.com>
2025-10-06 14:23:24 -0300

    Aula 157 - Estudando server actions (use server)

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+// page.tsx = index.html
+// app/page.tsx -> /
+// app/about/page.tsx -> /about
+
+import { PostFeatured } from '@/components/PostFeatured';
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
+
+// page.tsx (server) -> menu.tsx (server) -> link (client)
+
+export default async function HomePage() {
+  return (
+    <>
+      <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
+        <PostFeatured />
+        <PostsList />
+      </Suspense>
+    </>
+  );
+}
---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,30 @@
+import {
+  format,
+  formatDistanceToNow as datefnsFormatDistanceToNow,
+} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+export function formatDateTime(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+    locale: ptBR,
+  });
+}
+
+export function formatDistanceToNow(rawDate: string): string {
+  const date = new Date(rawDate);
+
+  return datefnsFormatDistanceToNow(date, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+}
+
+export function formatHour(timestampMs: number): string {
+  const date = new Date(timestampMs);
+
+  return format(date, 'HH:mm:ss', {
+    locale: ptBR,
+  });
+}
