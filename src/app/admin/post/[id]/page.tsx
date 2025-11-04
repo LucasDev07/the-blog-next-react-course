28e32919dad56eec50f176769d379e52618a88f3
Lucas <lukeamfernandes@gmail.com>
2025-11-04 13:30:36 -0300

    191. Estudando hook useActionState

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,11 +1,31 @@
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
+import { makePublicPost } from '@/dto/post/dto';
+import { findPostByIdAdmin } from '@/lib/post/queries/admin';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 export const dynamic = 'force-dynamic';
 
 type AdminPostPageProps = {
   params: Promise<{ id: string }>;
 };
 
+export const metadata: Metadata = {
+  title: 'Editar post',
+};
+
 export default async function AdminPostIdPage({ params }: AdminPostPageProps) {
   const { id } = await params;
+  const post = await findPostByIdAdmin(id).catch();
 
-  return <div className='py-16 text-6xl'>AdminPostIdPage {id}</div>;
+  if (!post) notFound();
+
+  const publicPost = makePublicPost(post);
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <h1 className='text-xl font-extrabold'>Editar Post</h1>
+      <ManagePostForm publicPost={publicPost} />
+    </div>
+  );
 }
